8a3a31172030a5c5ff704b75bcb4fe691cc968ff
jphafelin <josepablo.hafelin@gmail.com>
2023-01-13 19:57:38 +0000

    Entrega Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let textHTML = "<ul class='list-group'>";
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let dom = "<li class='list-group-item'>" + pronoun[i] + adj[j] + noun[k] + "</li>";
+
+        textHTML = textHTML + dom;
+        console.log(dom);
+      }
+    }
+  }
+
+  textHTML = textHTML + "</ul>";
+
+  let doms = document.querySelector("#dominios");
+  doms.innerHTML = textHTML;
 };
